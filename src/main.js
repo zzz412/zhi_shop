c15985a61d725d58f035e9153d6f8828f99cad12
某渣粗 <mouzhacu@163.com>
2022-04-11 17:16:24 +0800

    样式初始化 + 配置

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 初始化CSS
+import 'normalize.css'
+// 重置CSS
+import './styles/reset.scss'
+
 Vue.config.productionTip = false
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 关闭eslint报错
+  lintOnSave: false,
+  // 自定义开发服务器
+  devServer: {
+    open: true
+  }
 })
